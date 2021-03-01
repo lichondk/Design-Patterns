facc47882b3df6a7e6162982f2475afda83764c9
lichondk <lichondk@gmail.com>
2021-03-01 12:43:34 +0100

    solid patterns open-close and single responsibility

---
+++ soild/soild/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soild 
+{
+    #region responsible
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+        //keeping entries and Persistence
+        #region too much Responsibility for the journal class 
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public void Load(string filename)
+        {
+
+        }
+
+        public void Load(Uri uri)
+        {
+
+        }
+        #endregion
+    }
+
+    //for example
+    //instead move the persistence to a new class 
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I cried today.");
+            j.AddEntry("I ate a bug.");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\\Dokumenter\temo.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+    #endregion
+
+    //one class is responsible for one thing and has one reason to change.
+}
